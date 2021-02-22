8625e342ab62d179b5e22961fa38d5d94d0a962e
Jacek Głogosz <jacek.glogosz@gmail.com>
2021-02-22 19:19:54 +0100

    update of ts types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express-serve-static-core";
 import { JSONSchema4, JSONSchema6, JSONSchema7 } from "json-schema";
-import { ErrorObject, Options as AjvOptions } from "ajv";
+import Ajv, { ErrorObject, Options as AjvOptions } from "ajv";
 
 declare module "express-json-validator-middleware" {
 	type OptionKey = "body" | "params" | "query";
@@ -17,9 +17,10 @@ declare module "express-json-validator-middleware" {
 		| JSONSchema7;
 
 	export class Validator {
+		ajv: Ajv
 		constructor(options: AjvOptions);
 
-		validate(rules: List<ValidateFunction>): RequestHandler;
+		validate (rules: List<ValidateFunction>): RequestHandler;
 	}
 
 	export class ValidationError extends Error {
