1b5d5b4e3f06dca05998dcf67be8c59633fffa82
Josef <vaccekjo@gmail.com>
2026-03-28 22:23:05 +0100

    Fix optional Validator constructor typings (#126)
    * Fix optional Validator constructor typings
    
    * test: cover Validator consumer typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,32 +3,30 @@ import { RequestHandler } from "express-serve-static-core";
 import { JSONSchema4, JSONSchema6, JSONSchema7 } from "json-schema";
 import Ajv, { ErrorObject, Options as AjvOptions } from "ajv";
 
-declare module "express-json-validator-middleware" {
-	type OptionKey = "body" | "params" | "query";
+type OptionKey = "body" | "params" | "query";
 
-	type List<T> = {
-		[K in OptionKey]?: T;
-	};
+type List<T> = {
+	[K in OptionKey]?: T;
+};
 
-	type AllowedSchema =
-		| JSONSchema4
-		| JSONSchema6
-		| JSONSchema7;
+type AllowedSchema =
+	| JSONSchema4
+	| JSONSchema6
+	| JSONSchema7;
 
-	export type ValidateFunction =
-		| ((req: Request) => AllowedSchema)
-		| AllowedSchema;
+export type ValidateFunction =
+	| ((req: Request) => AllowedSchema)
+	| AllowedSchema;
 
-	export class Validator {
-		constructor(options: AjvOptions);
+export class Validator {
+	constructor(options?: AjvOptions);
 
-		ajv: Ajv;
+	ajv: Ajv;
 
-		validate(rules: List<ValidateFunction>): RequestHandler;
-	}
+	validate(rules: List<ValidateFunction>): RequestHandler;
+}
 
-	export class ValidationError extends Error {
-		constructor(validationErrors: List<ErrorObject[]>);
-		public validationErrors: List<ErrorObject[]>;
-	}
+export class ValidationError extends Error {
+	constructor(validationErrors: List<ErrorObject[]>);
+	public validationErrors: List<ErrorObject[]>;
 }
