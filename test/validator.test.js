288ec576e53e111dc010413940b593ecb9631e64
Atris <vaccekjo@gmail.com>
2026-03-28 22:00:19 +0100

    Add ValidationError regression coverage

--- test/validator.test.js
+++ test/validator.test.js
@@ -1,8 +1,27 @@
 const { test } = require("tap");
 const Ajv = require("ajv");
 
-const { Validator } = require("../src");
+const { ValidationError, Validator } = require("../src");
 
 test("Validator instance", async t => {
 	t.type(new Validator().ajv, Ajv, " property `ajv` should be an Ajv instance");
+});
+
+test("ValidationError instance", async t => {
+	const validationErrors = {
+		params: [
+			{
+				instancePath: "/id",
+				schemaPath: "#/properties/id/maxLength",
+				keyword: "maxLength",
+				params: { limit: 2 },
+				message: "should NOT be longer than 2 characters"
+			}
+		]
+	};
+
+	const validationError = new ValidationError(validationErrors);
+
+	t.equal(validationError.name, "JsonSchemaValidationError");
+	t.same(validationError.validationErrors, validationErrors);
 });
